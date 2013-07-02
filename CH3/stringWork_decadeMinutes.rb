eb90568b1ef29ea6192d82dea73d48aad2240120
John Landes <jmlandes@containerstore.com>
2013-07-01 22:26:08 -0500

    added working file for chapter 3 on strings
    single file with several examples based on decadeMinutes.rb from chapter 2

---
+++ CH3/stringWork_decadeMinutes.rb
@@ -0,0 +1,11 @@
+puts "the minutes in a decade is "
+puts (((60 * 24) * 365) * 10) 
+
+puts "there should be a possesion' "
+
+puts "pigs"*3
+
+puts "now single quote pigs"
+puts 'pigs'*3 
+
+puts "now single quote pigs concated together" + 'pigs'*3 
