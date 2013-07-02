d88a4f607c7c007fb9cffb2864d7e5858448ce71
John Landes <jmlandes@containerstore.com>
2013-07-01 22:49:18 -0500

    variable assignment project for chapter 4
    created another variation of minutesDecade for chapter 4 using varibales
    and assignments

---
+++ CH4/assignVars.rb
@@ -0,0 +1,11 @@
+
+subject =  "the minutes in a decade is "
+mins = 60
+hourInDay = 24
+daysInYear = 365
+yearsInDecade = 10
+
+minutesInDecade = (((mins * hourInDay) * daysInYear) * yearsInDecade)
+
+puts subject 
+puts minutesInDecade
