58220786548c573d7207f143ec4231666e0c95b8
John Landes <jmlandes@containerstore.com>
2013-07-22 14:49:20 -0500

    enhanced roman numeral to reduce code

--- CH9/lib/romanNumeral.rb
+++ CH9/lib/romanNumeral.rb
@@ -16,43 +16,31 @@
 	      print conversion(count,letter)
 	  end
 	end 
-	 
+
+iteration = [1000,500,100,50,10,5,1]	 
+result = []
+letters = { 1000 => 'M',
+					  500 => 'D',
+					  100 => 'C',
+					  50 => 'L',
+					  10 => 'X',
+					  5 => 'V',
+					1 => 'I' }
+
 puts "Enter your number for conversion to Roman Numbers "
 numIn = gets.chomp	
-
-thousands = division(numIn.to_i, 1000)
-thousand = thousands[0]
-remainder = thousands[1]
-
-fiveHundreds = division(remainder, 500)
-fiveHundred = fiveHundreds[0]
-remainder = fiveHundreds[1]
-
-hundreds = division(remainder, 100)
-hundred = hundreds[0]
-remainder = hundreds[1]
-
-fifties = division(remainder, 50)
-fifty = fifties[0]
-remainder = fifties[1]
-
-tens = division(remainder, 10)
-ten = tens[0]
-remainder = tens[1]
-
-fives = division(remainder, 5)
-five = fives[0]
-remainder = fives[1]
-
-ones = division(remainder, 1)
-one = ones[0]
-remainder = ones[1]
-
-romanResults(thousand, "M", true)
-romanResults(fiveHundred, "D",false)
-romanResults(hundred, "C",false)
-romanResults(fifty, "L",false)
-romanResults(ten, "X",false)
-romanResults(five, "V",false)
-romanResults(one, "I",false)
-
+remainder = numIn.to_i
+					  
+iteration.each {|divisor| 
+			stepResult = division(remainder,divisor)
+			result[divisor] = stepResult[0] 
+			remainder = stepResult[1] 
+		}
+	
+iteration.each {|divisor| 
+			if divisor == 1000
+			   romanResults(result[divisor], letters[divisor], true)
+			else
+			   romanResults(result[divisor], letters[divisor], false)
+			end 
+		}
